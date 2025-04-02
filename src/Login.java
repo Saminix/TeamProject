61073cec814830297b69c3e726a4014b5dd6a77a
Sameen-Iqbal <Sameen.iqbal@city.ac.uk>
2025-04-02 04:29:43 +0100

    Significant changes to the db file location. needs to be in its own package for better usability. there is now located in the db file. other classes that use this db connection have now correct import statements.

--- src/Login.java
+++ src/Login.java
@@ -1,10 +1,8 @@
+import db.dbConnection;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * Login
